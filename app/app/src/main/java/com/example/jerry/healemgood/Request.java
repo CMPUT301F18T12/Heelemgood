13e89e4023b6f38ea6747f954192f94d2674d85e
joeyUalberta <wong1@ualberta.ca>
2018-10-29 15:39:56 -0600

    Request class created

---
+++ app/app/src/main/java/com/example/jerry/healemgood/Request.java
@@ -0,0 +1,15 @@
+package com.example.jerry.healemgood;
+
+public class Request {
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    private int senderId;
+    private String msg;
+
+}
