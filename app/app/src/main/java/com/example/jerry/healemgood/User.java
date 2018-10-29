9afc4ac6f051d00d5eb744c51e67bc66d53e00be
joeyUalberta <wong1@ualberta.ca>
2018-10-29 15:29:02 -0600

    User class added

---
+++ app/app/src/main/java/com/example/jerry/healemgood/User.java
@@ -0,0 +1,72 @@
+package com.example.jerry.healemgood;
+
+import java.util.Date;
+
+public abstract class User {
+
+    protected String userid;
+    protected String password;
+    protected String fullName;
+    protected int phoneNum;
+    protected String email;
+    protected Date birthday;
+    protected char gender;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+
+}
