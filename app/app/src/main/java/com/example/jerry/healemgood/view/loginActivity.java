32d73ed3cdd67c52ca8d2f1295077b73ec6efca2
Jerry_Bao <jerrybao1998@outlook.com>
2018-11-06 22:23:18 -0700

    Added in login skeleton code

---
+++ app/app/src/main/java/com/example/jerry/healemgood/view/loginActivity.java
@@ -0,0 +1,15 @@
+package com.example.jerry.healemgood.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jerry.healemgood.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
