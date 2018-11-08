f915f54fa8f30e7efe497ff996294e3a0a5047e2
Jerry_Bao <jerrybao1998@outlook.com>
2018-11-07 21:20:17 -0700

    Refactored View

--- app/app/src/main/java/com/example/jerry/healemgood/view/UserViews/loginActivity.java
+++ app/app/src/main/java/com/example/jerry/healemgood/view/UserViews/loginActivity.java
@@ -1,7 +1,7 @@
-package com.example.jerry.healemgood.view;
+package com.example.jerry.healemgood.view.UserViews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.jerry.healemgood.R;
 
