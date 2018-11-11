34baa821f371f17f46b281cd13e943e78e561eff
Jerry_Bao <jerrybao1998@outlook.com>
2018-11-11 15:09:40 -0700

    Added in search by password and username

--- app/app/src/main/java/com/example/jerry/healemgood/controller/UserCreationController.java
+++ app/app/src/main/java/com/example/jerry/healemgood/controller/UserCreationController.java
@@ -10,6 +10,7 @@ import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,23 +19,26 @@ import io.searchbox.core.Index;
 import io.searchbox.core.Search;
 import io.searchbox.core.SearchResult;
 
+import static com.example.jerry.healemgood.controller.ProblemController.setClient;
+
 //TODO: Fix the issue of setClient() being local, assign it a singleton and make it global
 public class UserCreationController {
-    private static JestDroidClient client=null;
+    private static JestDroidClient client = null;
+
 
     // Add a User to the database
-    public static class addUserTask extends AsyncTask<User,Void,Void> {
-        protected Void doInBackground(User... users){
+    public static class addUserTask extends AsyncTask<User, Void, Void> {
+        protected Void doInBackground(User... users) {
             setClient();
             User user = users[0];
             Index index = new Index.Builder(user).index("Name-Jeff").type("user").build();
-            try{
+            try {
                 DocumentResult result = client.execute(index);
-                if (result.isSucceeded()){
+                if (result.isSucceeded()) {
                     System.out.println("OK, it worked");
                     user.setUserId(result.getId());
                 }
-            }catch (IOException e){
+            } catch (IOException e) {
                 System.out.println("lmao, I dun goofed here boyo");
                 e.printStackTrace();
             }
@@ -42,40 +46,72 @@ public class UserCreationController {
         }
     }
 
-    // Class used to find the user from the password and username
-    public static class searchUserPasswordandUsername extends AsyncTask<String,Void,ArrayList<Record>> {
-        protected ArrayList<Record> doInBackground(String... keywords) {
+
+    // Searches to see if a username already exists
+    // Returns the object associated with that username
+    public static class searchUsername extends AsyncTask<String, Void, ArrayList<User>> {
+        protected ArrayList<User> doInBackground(String... username) {
             setClient();
-            String query = "{\n" +
+            String usernameQuery = "{\n" +
                     "    \"query\": {\n" +
-                    "        \"multi_match\" :{ \n"+
-                    "               \"query\": \""+keywords[0]+"\",\n"+
-                    "               \"fields\": [ \"title\",\"description\"]\n"+
+                    "        \"multi_match\" :{ \n" +
+                    "               \"query\": \"" + username[0] + "\",\n" +
+                    "               \"fields\": [ \"userId\"]\n" +
                     "    }\n" +
                     "}";
-            ArrayList<Record> records = new ArrayList<Record>();
-            Search search = new Search.Builder(query).addIndex("Name-Jeff").addType("record").build();
-            try{
-                SearchResult result = client.execute(search);
-                if(result.isSucceeded()){
-                    Log.d("Name-Jeff","Record searched");
-                    List<Record> resultList = result.getSourceAsObjectList(Record.class);
-                    records.addAll(resultList);
+            Search searchUsername = new Search.Builder(usernameQuery).addIndex("Name-Jeff").addType("user").build();
+            ArrayList<User> userArrayList = new ArrayList<>();
+            try {
+                SearchResult usernameResult = client.execute(searchUsername);
+                if (usernameResult.isSucceeded()) {
+                    List<User> userList = usernameResult.getSourceAsObjectList(User.class);
+                    userArrayList.addAll(userList);
                 }
-            }catch(IOException e){
-                Log.d("Joey Error"," IOexception when executing client");
+            } catch (IOException e) {
+                Log.d("Error", " IOexception when executing client");
             }
-            return records;
+            return userArrayList;
         }
     }
 
+
+    // Class used to find the user from the password and username
+    // Assume the positioning is username, then password
+    // Returns the user object if username and passwords match
+    public static class searchUserPasswordandUsername extends AsyncTask<String, Void, ArrayList<User>> {
+        protected ArrayList<User> doInBackground(String... keywords) {
+            setClient();
+            ArrayList<User> userList = new UserCreationController.searchUsername().doInBackground(keywords[0]);
+            String passwordQuery = "{\n" +
+                    "    \"query\": {\n" +
+                    "        \"multi_match\" :{ \n" +
+                    "               \"query\": \"" + keywords[0] + "\",\n" +
+                    "               \"fields\": [ \"password\"]\n" +
+                    "    }\n" +
+                    "}";
+            Search searchPassword = new Search.Builder(passwordQuery).addIndex("Name-Jeff").addType("user").build();
+            ArrayList<User> userArrayList = new ArrayList<>();
+            try {
+                SearchResult passwordResult = client.execute(searchPassword);
+                if (userList.size() != 0 && passwordResult.isSucceeded()) {
+                    List<User> users = passwordResult.getSourceAsObjectList(User.class);
+                    userArrayList.addAll(users);
+                }
+            } catch (IOException e) {
+                Log.d("Error", " IOexception when executing client");
+            }
+            return userArrayList;
+        }
+    }
+
+
     // Set the client, duplicate code
-    private static void setClient(){
-        if(client==null){
+    private static void setClient() {
+        if (client == null) {
             DroidClientConfig config = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080/").build();
             JestClientFactory factory = new JestClientFactory();
             factory.setDroidClientConfig(config);
-            client=(JestDroidClient)factory.getObject();
+            client = (JestDroidClient) factory.getObject();
         }
     }
 }
