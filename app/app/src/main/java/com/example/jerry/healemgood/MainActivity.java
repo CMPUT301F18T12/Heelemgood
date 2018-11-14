fd54a877f0b614880ed49c9618f997c36d6cc92b
xiacijie <cijie@ualberta.ca>
2018-11-13 23:42:58 -0700

    mainactivity for login

--- app/app/src/main/java/com/example/jerry/healemgood/MainActivity.java
+++ app/app/src/main/java/com/example/jerry/healemgood/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.patient_user);
+        setContentView(R.layout.activity_login);
     }
 }
