a00f15a098db9262ca61f40ac165ee935f4ce6e7
xiacijie <cijie@ualberta.ca>
2018-11-15 09:55:16 -0700

    taking photo function with display

--- app/app/src/main/java/com/example/jerry/healemgood/view/UserViews/PatientAddRecordActivity.java
+++ app/app/src/main/java/com/example/jerry/healemgood/view/UserViews/PatientAddRecordActivity.java
@@ -6,12 +6,16 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.jerry.healemgood.R;
+import com.example.jerry.healemgood.view.UserViews.adapter.ImageAdapter;
 
 public class PatientAddRecordActivity extends AppCompatActivity {
 
@@ -51,6 +55,19 @@ public class PatientAddRecordActivity extends AppCompatActivity {
         });
 
 
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(PatientAddRecordActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     //https://developer.android.com/training/camera/photobasics
@@ -68,8 +85,8 @@ public class PatientAddRecordActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView photoView = findViewById(R.id.imageView);
-            photoView.setImageBitmap(imageBitmap);
+//            ImageView photoView = findViewById(R.id.imageView);
+//            photoView.setImageBitmap(imageBitmap);
         }
     }
 
---
+++ app/app/src/main/java/com/example/jerry/healemgood/view/UserViews/adapter/ImageAdapter.java
@@ -0,0 +1,62 @@
+package com.example.jerry.healemgood.view.UserViews.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.example.jerry.healemgood.R;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(300, 300));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(5, 5, 5, 5);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.body_map, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+}
