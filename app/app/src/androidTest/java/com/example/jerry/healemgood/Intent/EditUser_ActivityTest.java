e83958950f133f5ecd04c4e66672f9b0e458ee70
Jerry_Bao <jerrybao1998@outlook.com>
2018-11-19 20:45:46 -0700

    Added in test for user updates

--- app/app/src/androidTest/java/com/example/jerry/healemgood/Intent/EditUser_ActivityTest.java
+++ app/app/src/androidTest/java/com/example/jerry/healemgood/Intent/EditUser_ActivityTest.java
@@ -1,38 +1,28 @@
 package com.example.jerry.healemgood.Intent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.test.ActivityInstrumentationTestCase;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import com.example.jerry.healemgood.MainActivity;
 import com.example.jerry.healemgood.R;
 import com.example.jerry.healemgood.controller.UserController;
 import com.example.jerry.healemgood.model.user.User;
-import com.example.jerry.healemgood.MainActivity;
 import com.example.jerry.healemgood.view.UserActivities.AccountCreationActivity;
 import com.example.jerry.healemgood.view.UserViews.PatientAllProblemActivity;
+import com.example.jerry.healemgood.view.UserViews.PatientUserActivity;
 import com.robotium.solo.Solo;
-// Source: https://www.youtube.com/watch?v=T_8euppCz3k Accessed 2018-11-18
 
-/**
- * Handles logging in
- * Creates an account, uses it to log in, and deletes it afterwards
- * Tests Main activity, AccountCreation Activity
- * @author WeakMill98
- * @version 1.0
- * @see ActivityInstrumentationTestCase2
- * @see AccountCreationActivity
- * @see MainActivity
- * @since 1.0
- */
+public class EditUser_ActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    Solo solo;
 
-public class Main_AccountCreation_ActivityTest extends ActivityInstrumentationTestCase2<com.example.jerry.healemgood.MainActivity> {
-    private Solo solo;
-
-    public Main_AccountCreation_ActivityTest(){
+    public EditUser_ActivityTest(){
         super("com.example.jerry.healemgood.Intent",
-                com.example.jerry.healemgood.MainActivity.class);
+                MainActivity.class);
     }
 
     public void setUp() throws Exception {
@@ -46,20 +36,10 @@ public class Main_AccountCreation_ActivityTest extends ActivityInstrumentationTe
         super.tearDown();
     }
 
-    public void testNoUserNameLogin(){
-        // Clicking on sign in without a username will not sign in
-        solo.clickOnButton("Sign In");
-        solo.assertCurrentActivity("Check on login", MainActivity.class);
-    }
-
-    public void testLogin() {
-        solo.assertCurrentActivity("Check on login", MainActivity.class);
-        // Just to check that the code is running
-        // fail()
-
+    public void testEditUser(){
         // Click on create a new user
         // Assert that you go to the right page
-        TextView createAccount = (TextView) solo.getView(R.id.createAccountTextView);
+        /*TextView createAccount = (TextView) solo.getView(R.id.createAccountTextView);
         solo.clickOnView(createAccount);
         solo.assertCurrentActivity("Check on login", AccountCreationActivity.class);
 
@@ -82,13 +62,21 @@ public class Main_AccountCreation_ActivityTest extends ActivityInstrumentationTe
         solo.clickOnView(radioButton);
 
         // Create the account
-        solo.clickOnButton("Create");
+        solo.clickOnButton("Create");*/
 
         // Enter the credentials and enter the application
         EditText loginCredentials = (EditText) solo.getView(R.id.userIdEditText);
         solo.enterText(loginCredentials, "TestGUY12345");
         solo.clickOnButton("Sign In");
         solo.assertCurrentActivity("Check on login", PatientAllProblemActivity.class);
+
+        // Edit a field of the user class
+        solo.sleep(5000);
+        ViewGroup tabs = (ViewGroup) solo.getView(R.id.nav_view);
+        View userIcon = tabs.getChildAt(0);
+        solo.clickOnView(userIcon);
+
+        //solo.("PatientUserActivity");
 
         // Delete the test user
         User user;
