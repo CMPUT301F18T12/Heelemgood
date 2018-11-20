3cab12e93daf998d1d0e70b9f186285a5a1801cb
Jerry_Bao <jerrybao1998@outlook.com>
2018-11-19 19:41:42 -0700

    Added in skeleton for problem intent testing

---
+++ app/app/src/androidTest/java/com/example/jerry/healemgood/Intent/Problem_ActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.jerry.healemgood.Intent;
+
+import android.test.ActivityInstrumentationTestCase;
+
+import com.example.jerry.healemgood.view.UserViews.PatientAllProblemActivity;
+
+public class Problem_ActivityTest extends ActivityInstrumentationTestCase<PatientAllProblemActivity> {
+    public Problem_ActivityTest(){
+        super("com.example.jerry.healemgood.Intent",
+                PatientAllProblemActivity.class);
+    }
+}
