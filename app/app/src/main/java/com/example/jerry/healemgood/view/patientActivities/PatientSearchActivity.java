cd2883befacce8962f48222cbd1978c0bd9aea9b
xiacijie <cijie@ualberta.ca>
2018-11-26 01:40:23 -0700

    add spinner

--- app/app/src/main/java/com/example/jerry/healemgood/view/patientActivities/PatientSearchActivity.java
+++ app/app/src/main/java/com/example/jerry/healemgood/view/patientActivities/PatientSearchActivity.java
@@ -11,6 +11,8 @@ package com.example.jerry.healemgood.view.patientActivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.jerry.healemgood.R;
 
@@ -35,5 +37,16 @@ public class PatientSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_search);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.search_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
     }
 }
