e0b81cc3bf6c942fd289ebab691c985a452505e5
Tianqi Wang <twang2@ualberta.ca>
2018-11-27 16:56:21 -0700

    care provider not done

---
+++ app/app/src/main/java/com/example/jerry/healemgood/view/adapter/PatientAdapter.java
@@ -0,0 +1,98 @@
+/*
+ *  Class Name: ProblemAdapter
+ *
+ *  Version: Version 1.0
+ *
+ *  Date: November 17, 2018
+ *
+ *  Copyright (c) Team 12, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at the University of Alberta
+ */
+package com.example.jerry.healemgood.view.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.jerry.healemgood.R;
+import com.example.jerry.healemgood.model.record.Record;
+import com.example.jerry.healemgood.model.user.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An adapter class that helps with records
+ *
+ * @author xiacijie
+ * @version 1.0
+ * @since 1.0
+ */
+public class PatientAdapter extends ArrayAdapter<Patient> {
+
+    private Context mContext;
+    private int layout;
+    private ArrayList<Patient> patients;
+
+    /**
+     * Creates a RecordAdapter
+     *
+     * @param c
+     * @param layout
+     * @param patients
+     */
+    public PatientAdapter(Context c, int layout, ArrayList<Patient> patients){
+        super(c,layout,patients);
+        this.layout = layout;
+        this.mContext = c;
+        this.patients = patients;
+    }
+
+
+    /**
+     * Refreshes the adapter
+     *
+     * @param records
+     */
+    public void refreshAdapter(List<Record> records){
+        this.patients.clear();
+        this.patients.addAll(patients);
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Gets and returns the view
+     *
+     * @param position
+     * @param convertView
+     * @param viewGroup
+     * @return v
+     */
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(mContext);
+            v = vi.inflate(layout, null);
+        }
+
+        Patient p = getItem(position);
+
+        if (p != null){
+
+            TextView patientName = v.findViewById(R.id.patientName);
+            TextView patientUid = v.findViewById(R.id.patientUid);
+
+            patientName.setText(p.getFullName());
+            patientUid.setText(p.getUserId());
+        }
+        return v;
+    }
+
+
+}
---
+++ app/app/src/main/java/com/example/jerry/healemgood/view/careProviderActivities/CareProviderAllPatientActivity.java
@@ -0,0 +1,258 @@
+/*
+ *  Class Name: PatientAllProblemActivity
+ *
+ *  Version: Version 1.0
+ *
+ *  Date: November 17, 2018
+ *
+ *  Copyright (c) Team 12, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at the University of Alberta
+ */
+//TODO NOT YET FINISHED
+package com.example.jerry.healemgood.view.careProviderActivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.jerry.healemgood.R;
+import com.example.jerry.healemgood.config.AppConfig;
+import com.example.jerry.healemgood.controller.ProblemController;
+import com.example.jerry.healemgood.controller.SwipeDetector;
+import com.example.jerry.healemgood.model.problem.Problem;
+import com.example.jerry.healemgood.model.user.Patient;
+import com.example.jerry.healemgood.utils.SharedPreferenceUtil;
+import com.example.jerry.healemgood.view.adapter.PatientAdapter;
+import com.example.jerry.healemgood.view.adapter.ProblemAdapter;
+import com.example.jerry.healemgood.view.patientActivities.PatientAddProblemActivity;
+import com.example.jerry.healemgood.view.patientActivities.PatientAllRecordActivity;
+import com.example.jerry.healemgood.view.patientActivities.PatientMapModeActivity;
+import com.example.jerry.healemgood.view.patientActivities.PatientSearchActivity;
+import com.example.jerry.healemgood.view.patientActivities.PatientUserActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a PatientAllProblemActivity
+ * displays all problems and handles all functions relates to navigate bar
+ *
+ * @author xiacijie
+ * @version 1.0
+ * @see AppCompatActivity
+ * @since 1.0
+ */
+
+public class CareProviderAllPatientActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ArrayList<Patient> patients;
+    private ProblemAdapter problemAdapter;
+    private PatientAdapter patientAdapter;
+
+    /**
+     * Handles loading an older version of the activity
+     *
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.care_provider_patient_list);
+        /*
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Patients");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View hView =  navigationView.getHeaderView(0);
+        TextView nav_user = (TextView)hView.findViewById(R.id.drawerUserId);
+        TextView nav_email = (TextView)hView.findViewById(R.id.drawerEmail);
+        nav_user.setText(SharedPreferenceUtil.get(this,AppConfig.USERID));
+        nav_email.setText(SharedPreferenceUtil.get(this,AppConfig.EMAIL));
+        */
+
+
+        ListView mListView;
+
+        mListView = findViewById(R.id.careProviderPatientList);
+
+        loadPatients();
+
+
+        problemAdapter = new ProblemAdapter(this,R.layout.problems_list_view_custom_layout,problems);
+
+        mListView.setAdapter(problemAdapter);
+        final SwipeDetector swipeDetector = new SwipeDetector();
+        mListView.setOnTouchListener(swipeDetector);
+
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(swipeDetector.swipeDetected()) {
+                    if(swipeDetector.getAction() == SwipeDetector.Action.LR){
+                        deleteProblem(position);
+                    }
+                }
+                else{
+                    String pId = problems.get(position).getpId();
+                    Intent intent = new Intent(CareProviderAllPatientActivity.this,PatientAllRecordActivity.class);
+                    intent.putExtra(AppConfig.PID,pId);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+    }
+
+    /**
+     * refresh problems
+     *
+     */
+
+    @Override
+    protected void onResume(){
+
+        super.onResume();
+        loadProblems();
+        problemAdapter.refreshAdapter(problems);
+
+    }
+
+    /**
+     * load problems
+     *
+     */
+
+    private void loadPatients(){
+        ProblemController.searchByPatientIds(SharedPreferenceUtil.get(this,AppConfig.USERID));
+        try{
+            problems = new ProblemController.SearchProblemTask().execute().get();
+
+
+        }
+        catch (Exception e){
+            Log.d("Error","Fail to get the problems");
+            problems = new ArrayList<Problem>();
+        }
+
+
+
+    }
+
+    /**
+     * delete problems
+     *
+     */
+
+    private void deleteProblem(int i){
+
+        try {
+            new ProblemController.DeleteProblemTask().execute(problems.get(i)).get();
+            problems.remove(i);
+            // notify changes
+            problemAdapter.refreshAdapter(problems);
+        }
+        catch (Exception e){
+            Log.d("ERROR","FAIL to delete problem");
+        }
+
+    }
+
+
+    /**
+     *  close drawer
+     *
+     */
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * adds items to the action bar
+     *
+     * @param menu Menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.patient_all_problem, menu);
+        return true;
+    }
+    /**
+     * Handle action bar item clicks.
+     *
+     * @param item MenuItem
+     */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar will automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Handle navigation view item clicks.
+     *
+     * @param item MenuItem
+     */
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        if (id == R.id.navigation_search) {
+            startActivity(new Intent(getApplicationContext(),PatientSearchActivity.class));
+
+        } else if (id == R.id.navigation_user) {
+            startActivity(new Intent(getApplicationContext(),PatientUserActivity.class));
+        }
+        else if (id == R.id.navigation_map){
+            startActivity(new Intent(getApplicationContext(),PatientMapModeActivity.class));
+        }
+
+
+        return true;
+    }
+}
